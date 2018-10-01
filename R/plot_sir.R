131b8684bd60596713c5886cc6692b749b953834
Grant Adams <adamsgd@uw.edu>
2018-10-01 12:16:23 -0700

    Updated plot_sir to include IOAs
    The plot_sir function now plots the IOAs with CIs and the predicted IOAs with CIs.

--- R/plot_sir.R
+++ R/plot_sir.R
@@ -10,15 +10,12 @@
 #'   function.
 #'
 #' @return Returns and saves a figure with the population trajectory.
-plot_trajectory <- function(SIR, object = "USERDEFINED", file_name = "NULL") {
+plot_sir <- function(SIR, file_name = "NULL") {
 
-    # Change name if not supplied
-    if(object == "USERDEFINED"){
-        object == "trajectory_summary"
-    }
-
+    # Extract SIR objects
     x <- SIR$resamples_trajectories
     abs.abundance <- SIR$inputs$abs.abundance
+    rel.abundance <- SIR$inputs$rel.abundance
     catch.data <- SIR$inputs$catch.data
     N_priors <- SIR$inputs$priors_N_obs$pars
 
@@ -47,12 +44,17 @@ plot_trajectory <- function(SIR, object = "USERDEFINED", file_name = "NULL") {
     ymax <- max(c(max(output_summary[2:6, ]), abs.abundance$N.obs, abs.abundance$Lower95, abs.abundance$Upper95, N_priors))
     ymin <- 0
 
+    if(!is.null(rel.abundance)){
+        rel.abundance$Upper95 <- qlnorm(0.975, log(rel.abundance$IA.obs), rel.abundance$Sigma)
+        rel.abundance$Lower95 <- qlnorm(0.025, log(rel.abundance$IA.obs), rel.abundance$Sigma)
+    }
 
-    # Plot it
+
+    # Plot trajectory
     for(i in 1:2){
         if(i == 1){
-        filename <- paste0(file_name, "_", object, ".tiff")
-        tiff( file = filename , width=169 / 25.4, height = 100 / 25.4, family = "serif", units = "in", res = 300)
+            filename <- paste0(file_name, "_trajectory_summary", ".tiff")
+            tiff( file = filename , width=169 / 25.4, height = 100 / 25.4, family = "serif", units = "in", res = 300)
         }
 
         # Plot configuration
@@ -84,6 +86,96 @@ plot_trajectory <- function(SIR, object = "USERDEFINED", file_name = "NULL") {
                 x1 = abs.abundance$Year,
                 y1 = abs.abundance$Upper95,
                 length=0.05, angle=90, code=3, lwd = 3)
+
         if(i == 1){ dev.off()}
     }
+
+
+    # Plot IOA
+    IA.yrs <- rel.abundance$Year
+    IA.yr.range <- c((min(IA.yrs) - 1):(max(IA.yrs) + 1)) # Range +- 1 of IOA years
+
+    # Predict IOA
+    N_hat <- SIR$resamples_trajectories[, paste0("N_", IA.yr.range)] # Estimates of N within IOA years
+    q_cols <- grep("q_IA", colnames(SIR$resamples_output)) # Columns of resample Q estimates
+    q_est <- SIR$resamples_output[, q_cols]
+
+
+    IA_pread <- list()
+    IA_summary <- list()
+    for(i in 1:length(q_cols)){
+        # Predict
+        IA_pread[[i]] <- N_hat * q_est
+
+        # Summarize
+        IA_summary[[i]] <-  matrix(nrow = length(row_names), ncol = dim(IA_pread[[i]])[2])
+        IA_summary[[i]][1, ] <- sapply(IA_pread[[i]], mean)
+        IA_summary[[i]][2:6, ] <- sapply(IA_pread[[i]], quantile, probs= c(0.5, 0.025, 0.975, 0.05, 0.95))
+        IA_summary[[i]][7, ] <- sapply(IA_pread[[i]], min)
+        IA_summary[[i]][8, ] <- sapply(IA_pread[[i]], max)
+        IA_summary[[i]][9, ] <- sapply(IA_pread[[i]], length)
+
+
+
+        IA_summary[[i]] <- data.frame(IA_summary[[i]])
+        names(IA_summary[[i]]) <- paste0("IA", i, "_", IA.yr.range)
+        row.names(IA_summary[[i]]) <- row_names
+    }
+
+
+
+    ymax <- max(c(sapply(IA_summary, function(x) max(x[2:6,])), rel.abundance$Lower95, rel.abundance$Upper95))
+    ymin <- 0
+
+    if(!is.null(rel.abundance)){
+        for(j in 1:2){
+            if(j == 1){
+                filename <- paste0(file_name, "_IOA_fit", ".tiff")
+                tiff( file = filename , width=169 / 25.4, height = 100 / 25.4, family = "serif", units = "in", res = 300)
+            }
+
+            par(mfrow = c(1,length(IA_summary)))
+
+            # Loop through inices
+            for(i in 1:length(IA_summary)){
+                rel.abundance.sub <- rel.abundance[which(rel.abundance$Index == i),]
+
+                # Plot configuration
+                par( mar=c(3, 3 , 0.5 , 0.25) , oma=c(0 , 0 , 0 , 0), tcl = -0.35, mgp = c(1.75, 0.5, 0))
+                plot(y = NA, x = NA,
+                     ylim = c(ymin, ymax),
+                     xlim = c(min(IA.yr.range), max(IA.yr.range)),
+                     xlab = "Year", ylab = "Relative abundance")
+
+
+                # Credible interval
+                polygon(
+                    x = c(IA.yr.range, rev(IA.yr.range)),
+                    y = c(IA_summary[[i]][3, ],rev(IA_summary[[i]][4, ])),
+                    col = "Grey80", border = NA) # 95% CI
+
+                polygon( x = c(IA.yr.range, rev(IA.yr.range)),
+                         y = c(IA_summary[[i]][5, ], rev(IA_summary[[i]][6, ])),
+                         col = "Grey60", border = NA) # 90% CI
+
+
+                # Median and catch series
+                lines( x = IA.yr.range, y = IA_summary[[i]][2, ], lwd = 3) # Median
+
+
+                # Relative abundance
+                points( x = rel.abundance.sub$Year,
+                        y = rel.abundance.sub$IA.obs,
+                        col = 1, pch = 16, cex = 2)
+                arrows( x0 = rel.abundance.sub$Year,
+                        y0 = rel.abundance.sub$Lower95,
+                        x1 = rel.abundance.sub$Year,
+                        y1 = rel.abundance.sub$Upper95,
+                        length=0.05, angle=90, code=3, lwd = 3, col = 1)
+
+            }
+            if(j == 1){ dev.off()}
+        }
+    }
+
 }
