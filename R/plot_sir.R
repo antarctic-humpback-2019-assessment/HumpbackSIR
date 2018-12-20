cc2dcda1c64661c0761c770b4f781608c1aed353
Grant Adams <adamsgd@uw.edu>
2018-12-19 21:33:33 -0600

    added reference and priors to plots

--- R/plot_sir.R
+++ R/plot_sir.R
@@ -7,9 +7,10 @@
 #' @param SIR A fit SIR model
 #' @param file_name name of a file to identified the files exported by the
 #'   function.
+#' @param Reference A fit SIR model for the reference case
 #'
 #' @return Returns and saves a figure with the population trajectory.
-plot_trajectory <- function(SIR, file_name = "NULL") {
+plot_trajectory <- function(SIR, Reference = NULL, file_name = "NULL") {
 
 
     # Extract SIR objects
@@ -35,6 +36,20 @@ plot_trajectory <- function(SIR, file_name = "NULL") {
     output_summary <- data.frame(output_summary)
     names(output_summary) <- names(x)
     row.names(output_summary) <- row_names
+
+    if(!is.null(Reference)){
+        ref <- Reference$resamples_trajectories
+        reference_summary <- matrix(nrow = length(row_names), ncol = dim(ref)[2])
+        reference_summary[1, ] <- sapply(ref, mean)
+        reference_summary[2:6, ] <- sapply(ref, quantile, probs= c(0.5, 0.025, 0.975, 0.05, 0.95))
+        reference_summary[7, ] <- sapply(ref, min)
+        reference_summary[8, ] <- sapply(ref, maref)
+        reference_summary[9, ] <- sapply(ref, length)
+        reference_summary <- data.frame(reference_summary)
+        names(reference_summary) <- names(ref)
+        row.names(reference_summary) <- row_names
+        ref_years <- as.numeric(gsub("N_", "", colnames(reference_summary)))
+    }
 
     # Extract catch trajectories
     catch_summary <- matrix(nrow = length(row_names), ncol = dim(catch.data)[2])
@@ -79,6 +94,11 @@ plot_trajectory <- function(SIR, file_name = "NULL") {
 
         # Median
         lines( x = Years, y = output_summary[2, ], lwd = 3) # Median
+
+        # Reference median
+        if(!is.null(Reference)){
+            lines( x = ref_years, y = reference_summary[2, ], lwd = 3, lty = 3) # Median
+        }
 
         # Catch trajectory
         # Credible interval
@@ -230,15 +250,31 @@ plot_ioa <- function(SIR, file_name = "NULL"){
 #' @param file_name name of a file to identified the files exported by the
 #'   function.
 #' @param multiple_sirs Logical whether or not multiple SIRS are provided as a list.
+#' @param lower Vector of lower bounds for x-axis
+#' @param upper Vector of upper bounds for x-axis
+#' @param priors Default = NULL, wether realized priors are included in SIR
+#' @param reference Default = NULL, wether reference case is included in SIR
 #'
 #' @return Returns and saves a figure with the posterior densities of parameters.
-plot_density <- function(SIR, file_name = "NULL", multiple_sirs = FALSE, lower = NULL, upper = NULL){
+plot_density <- function(SIR, file_name = "NULL", multiple_sirs = FALSE, lower = NULL, upper = NULL, prior_list = NULL, inc_reference = FALSE){
 
     if(multiple_sirs == FALSE){
         sir_list <- list(SIR)
     }
     if(multiple_sirs == TRUE){
         sir_list <- SIR
+    }
+
+
+    posteriors_lwd <- rep(3, length(posterior_dens))
+    posteriors_lty <- c(1, 1:(length(posterior_dens)-1))
+    posteriors_col <- c("grey", rep(1, length(posterior_dens)-1))
+
+    if(!is.null(priors)){
+        posteriors_lwd <- c(posteriors_lwd, rep(1, length(priors)))
+        posteriors_lty <- c(posteriors_lty, c(1, 1:(length(priors)-1)))
+        posteriors_col <- c(posteriors_col, c("grey", rep(1, length(priors)-1)))
+        sir_list <- c(sir_list, priors)
     }
 
     # Vars of interest
@@ -265,9 +301,6 @@ plot_density <- function(SIR, file_name = "NULL", multiple_sirs = FALSE, lower =
             for(k in 1:length(sir_list)){
                 posterior_dens[[k]] <- density(sir_list[[k]]$resamples_output[,vars[i]])
             }
-            posteriors_lwd <- rep(3, length(posterior_dens))
-            posteriors_lty <- c(1, 1:(length(posterior_dens)-1))
-            posteriors_col <- c("grey", rep(1, length(posterior_dens)-1))
 
             # Extract prior densities
             # prior_dens <- list()
